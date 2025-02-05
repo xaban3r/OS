21a1dc9f7e9f3402e8d5f08308cff1ad29194a9a
Aleksey <nstr3@mail.ru>
2025-02-05 20:14:51 +0500

    i task done

---
+++ lab1/i.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [[ "$1" -gt "$2"  ]]
+then
+	if
+		[[ "$1" -lt "$3"  ]]
+	then echo "$3"
+	else echo "$1"
+	fi
+else if [[ "$2" -gt "$3"  ]]
+	then echo "$2"
+	else echo "$3"
+	fi
+fi
